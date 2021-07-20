03c0bcec8401a95b247ae95e67881e636c11898f
Mário Silva <mario.silva@utrust.com>
2021-07-20 12:23:55 +0100

    Action impl

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,66 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
+import head from 'lodash.head'
+import get from 'lodash.get'
+
+import {GetResponseTypeFromEndpointMethod} from '@octokit/types'
+
+const VALID_DEPLOYMENT_STATUSES = ['success', 'pending', 'in_progress']
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const ctx = github.context
+    const token = core.getInput('token', {required: true})
+    const sha = core.getInput('sha', {required: false}) || ctx.sha
+    const environment =
+      core.getInput('environment', {required: false}) || 'development'
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const client = github.getOctokit(token, {previews: ['flash', 'ant-man']})
 
-    core.setOutput('time', new Date().toTimeString())
+    type DeploymentsListResponseType = GetResponseTypeFromEndpointMethod<
+      typeof client.rest.repos.listDeployments
+    >
+    type DeploymentStatusesListResponseType = GetResponseTypeFromEndpointMethod<
+      typeof client.rest.repos.listDeploymentStatuses
+    >
+
+    const {
+      data: deployments
+    }: DeploymentsListResponseType = await client.rest.repos.listDeployments({
+      owner: ctx.repo.owner,
+      repo: ctx.repo.repo,
+      environment,
+      page: 1,
+      per_page: 1
+    })
+
+    const lastDeployment = head(deployments)
+
+    let isDeployable = true
+
+    if (lastDeployment) {
+      const {
+        data: statuses
+      }: DeploymentStatusesListResponseType = await client.rest.repos.listDeploymentStatuses(
+        {
+          owner: ctx.repo.owner,
+          repo: ctx.repo.repo,
+          deployment_id: lastDeployment.id
+        }
+      )
+
+      const lastStatus = get(statuses, '0.state')
+
+      isDeployable = !(
+        sha === lastDeployment.sha &&
+        VALID_DEPLOYMENT_STATUSES.includes(lastStatus)
+      )
+    }
+
+    core.setOutput('is_deployable', isDeployable.toString())
+    core.setOutput('sha', sha)
   } catch (error) {
+    core.error(error)
     core.setFailed(error.message)
   }
 }
